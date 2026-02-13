feb56bc76c23289a83d703d235686029f4c2c50f
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-13 19:34:43 +0530

    finished creating the create and get psot functions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uploadImage = require('./services/storage.service')
+const postModel = require('./models/post.model')
 const app = express();
 
 app.use(express.json());
@@ -8,10 +9,18 @@ app.use(express.json());
 const upload = multer({storage: multer.memoryStorage()});
 
 app.post('/create-post', upload.single('image'), async (req, res) => {
-    console.log(req.body)
     const result = await uploadImage(req.file.buffer);
-    console.log(result)
+    const post = new postModel({
+        image: result,
+        caption: req.body.caption
+    })
+    await post.save();
     res.status(201).json({ message: "Post created successfully", imageUrl: result });
 })
 
+
+app.get('/posts', async (req, res) => {
+    const posts = await postModel.find().sort({ createdAt: -1 });
+    res.status(200).json({ message: "Posts fetched successfully", posts });
+})
 module.exports = app;
